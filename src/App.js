0cfd607193f00fcfccb849125f340b132f67291d
Antonio Popovic <Antonio.Popovic@fesb.hr>
2022-02-11 15:14:19 +0100

    small changes visual

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
             <Header />
             <Form />
             <Result />
-            <Footer />
+            {/* <Footer /> */}
          </div>
       </MyProvider>
    );
