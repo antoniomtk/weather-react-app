adfea66c4f61760c66aa28eb81ca24b7c8f2e3cc
Antonio Popovic <Antonio.Popovic@fesb.hr>
2022-02-11 15:17:54 +0100

    small visual fixes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { MyProvider } from './context';
 import Header from './components/Header';
 import Result from './components/Result';
 import Form from './components/Form';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
